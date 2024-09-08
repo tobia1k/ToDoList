25a0c6d62b96821846c2720912668b7d546dcc74
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-09-07 21:56:06 -0400

    Add ToDo model
    This will be the model for what data should be stored in a to-do list item (Id, Name, IsCompleted, DueDate)

---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Name { get; set; }
+
+        public Boolean IsCompleted { get; set; }
+
+        [Display(Name = "Due by")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DueDate { get; set; }
+    }
+}
